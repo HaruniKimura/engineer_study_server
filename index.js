4ddb6d0a460e1c5ce975578b92adf93957d25967
haruni.kimurayoshiki <kimura@haru-ni.net>
2024-03-27 14:08:25 +0900

    API作成

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/a', (req, res) => {
+	res.send('Hello World\n');
+});
+
+app.get('/b', (req, res) => {
+	res.send('good morning\n');
+});
+
+app.listen(3000, () => {
+	console.log('Server listening on port 3000');
+});
